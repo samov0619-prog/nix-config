43c3a961a2ef093c76070af57879878dbdfb1bf4
samov0619-prog <samov0619@gmail.com>
2026-03-30 11:40:41 +0300

    add yazi init lua

--- home/core-set/shell.nix
+++ home/core-set/shell.nix
@@ -54,5 +54,13 @@
         }
       ];
     };
+    initLua = ''
+      -- show disk in status bar
+      Status:children_add(function()
+          local command = "df -kh .|awk '!/^Filesystem/{printf \" %s FREE \", $(NF-2)}'"
+          local info = ui.Span(io.popen(command):read('*a')):fg("green")
+          return info
+      end, 1500, Header.RIGHT)
+    '';
   };
 }
