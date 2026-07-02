09976996ddfd79992c1ecdb58e86aedfdf998424
samov0619-prog <samov0619@gmail.com>
2026-07-02 21:55:05 +0000

    fix auto-timezone

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -20,7 +20,8 @@
   # boot.loader.grub.efiInstallAsRemovable = true;
   # boot.loader.efi.efiSysMountPoint = "/boot/efi";
   # Define on which hard drive you want to install Grub.
-  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only services.automatic-timezoned.enable = true;
+  boot.loader.grub.device = "nodev"; # or "nodev" for efi only
+  services.automatic-timezoned.enable = true;
   boot.loader.grub.useOSProber = true;
 
   nix.gc = {
