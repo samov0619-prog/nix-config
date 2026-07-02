96de407b4fee81d56a969927e31dc4fef0e81a0b
samov0619-prog <samov0619@gmail.com>
2026-07-02 21:10:28 +0000

    home-manager.enable true

--- home/users/samov/default.nix
+++ home/users/samov/default.nix
@@ -7,6 +7,7 @@
 {
   home.username = username;
   home.homeDirectory = if pkgs.stdenv.isDarwin then "/Users/${username}" else "/home/${username}";
-
   home.stateVersion = "25.11";
+
+  programs.home-manager.enable = true;
 }
