0eb13caa4e3c2a65aa91c29b4350ce74c0a7c5d0
samov0619-prog <samov0619@gmail.com>
2026-07-09 12:47:32 +0300

    up to 26.05

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "NixOS & stanadalone home-manager flake";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-25.11";
+    nixpkgs.url = "nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "nixpkgs/nixpkgs-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
