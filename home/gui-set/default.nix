82f191c8c8d3a5532405890005984744f9f648c2
samov0619-prog <samov0619@gmail.com>
2026-07-20 21:56:22 +0300

    add chromium

--- home/gui-set/default.nix
+++ home/gui-set/default.nix
@@ -9,5 +9,8 @@
     enable = true;
     configPath = lib.mkIf (lib.versionOlder config.home.stateVersion "26.05") ".mozilla/firefox";
   };
+
+
+  programs.chromium.enable = true;
   programs.mpv.enable = true;
 }
