30e26d2b6b523b7356f85ddb9125828998aba7de
samov0619-prog <samov0619@gmail.com>
2026-08-08 00:01:08 +0300

    set GPG agent cache lifetime

--- home/core-set/security.nix
+++ home/core-set/security.nix
@@ -1,7 +1,13 @@
 { ... }:
 {
   programs.gpg.enable = true;
-  services.gpg-agent.enable = true;
+  services.gpg-agent = {
+    enable = true;
+    defaultCacheTtl = 3600;
+    maxCacheTtl = 3600;
+    defaultCacheTtlSsh = 3600;
+    maxCacheTtlSsh = 3600;
+  };
 
   programs.password-store.enable = true;
 }
